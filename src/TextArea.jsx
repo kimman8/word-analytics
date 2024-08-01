6f2458884cfae5b270c0da8dd6ea17e1435151ba
KIM YUEN <kim_man@live.com>
2024-08-01 16:40:15 +1000

    Add basic input handling and security checks to TextArea component.

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,22 +1,34 @@
 import { useState } from 'react';
+import Warning from './Warning';
 
 export default function TextArea() {
   const [text, setText] = useState('');
+  const [showWarning, setShowWarning] = useState(false);
+  const [warningText, setWarningText] = useState(
+    'Warning: Avoid using @ or <script> in your text'
+  );
   const handleChange = (e) => {
     let newText = e.target.value;
     if (newText.includes('<script>')) {
-      alert('No scripts allowed!');
+      setShowWarning(true);
       newText = newText.replace('<script>', '');
+      setWarningText('Warning: Avoid using <script> in your text');
+    } else if (newText.includes('@')) {
+      setShowWarning(true);
+      newText = newText.replace('@', '');
+      setWarningText('Warning: Avoid using @ in your text');
     }
     setText(newText);
   };
   return (
-    <textarea
-      placeholder="Enter some text..."
-      value={text}
-      className="textarea"
-      spellCheck="false"
-      onChange={handleChange}
-    />
+    <div className="textarea">
+      <textarea
+        placeholder="Enter some text..."
+        value={text}
+        spellCheck="false"
+        onChange={handleChange}
+      />
+      {showWarning && <Warning warningText={warningText} />}
+    </div>
   );
 }
